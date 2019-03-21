ea765b0984e961b110bb4ea47f45112a27646952
Danny <holler@dannyburns.me>
2019-03-21 00:10:57 -0700

    Primary nav is complete without dropdowns
    Site is almost done. Still needs final nav setup and animation.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,42 @@
 const menuTrigger = document.querySelector(".nav-burger-container"),
   burgerLine = document.querySelectorAll(".burger-line"),
-  menu = document.querySelector(".menu-container");
+  menu = document.querySelector(".menu-container"),
+  header = document.querySelector("header"),
+  navLinks = document.querySelectorAll(".navbar-link"),
+  topLogo = document.querySelector(".top-menu-logo"),
+  socialMenu = document.querySelector(".header-social-menu");
 
-menuTrigger.addEventListener("click", function() {
-  // console.log("this worked");
-
+menuTrigger.addEventListener("click", function(e) {
+  e.preventDefault();
   menu.classList.toggle("show");
   burgerLine.forEach(line => {
-    line.classList.toggle("show");
+    line.classList.toggle("active");
   });
+});
+
+// Page scrolling Event Listener
+window.addEventListener("scroll", function() {
+  const winScroll = window.pageYOffset;
+
+  if (winScroll > 50) {
+    header.classList.add("show");
+    topLogo.classList.add("active");
+    socialMenu.classList.add("active");
+    burgerLine.forEach(line => {
+      line.classList.add("show");
+    });
+    navLinks.forEach(link => {
+      link.classList.add("active");
+    });
+  } else if (winScroll < 50) {
+    header.classList.remove("show");
+    topLogo.classList.remove("active");
+    socialMenu.classList.remove("active");
+    burgerLine.forEach(line => {
+      line.classList.remove("show");
+    });
+    navLinks.forEach(link => {
+      link.classList.remove("active");
+    });
+  }
 });
