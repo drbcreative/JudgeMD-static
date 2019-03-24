78ca472f9a33dde2aedaf48b943845f9c16f41cd
Danny <holler@dannyburns.me>
2019-03-24 12:28:22 -0700

    Changed Animation
    Changed animation for Robin

--- js/app.js
+++ js/app.js
@@ -14,6 +14,25 @@ menuTrigger.addEventListener("click", function(e) {
   });
 });
 
+/* Page element animatin */
+function aniMate(element) {
+  // Assign El
+  const el = document.querySelectorAll(element);
+
+  // Event Listener
+  window.addEventListener("scroll", function() {
+    el.forEach(elem => {
+      if (elem.getBoundingClientRect().top < window.innerHeight / 2) {
+        elem.classList.add("animate");
+      }
+    });
+  });
+}
+aniMate(".fade-in");
+aniMate(".fade-in-left");
+aniMate(".fade-in-right");
+aniMate(".scale-down");
+
 // Page scrolling Event Listener
 window.addEventListener("scroll", function() {
   const winScroll = window.pageYOffset;
